a882c4f7be0780e41cf3beb7e1aac73402b1998b
Obnoxious718 <joshcf11@gmail.com>
2018-10-05 16:46:39 -0700

    commented out julia_quad

--- hc_julia.c
+++ hc_julia.c
@@ -22,7 +22,7 @@ int				julia_squared(t_view *view, double x, double y)
 			+ (view->x_shift / view->width);
 	y = ((4.0 * y / view->height - 2.0) / view->zoom)
 			+ (view->y_shift / view->height);
-	while (x * x + y * y <= 4.0 && i < view->max_iter)
+	while (x * x + y * y < 4.0 && i < view->max_iter)
 	{
 		new_x = x * x - y * y + view->fract->real;
 		y = 2 * x * y + view->fract->imag;
@@ -52,24 +52,24 @@ int				julia_cubed(t_view *view, double x, double y)
 	return (i);
 }
 
-int				julia_quad(t_view *view, double x, double y)
-{
-	int			i;
-	long double	new_x;
-
-	i = 0;
-	x = ((4.0 * x / view->width - 2.0) / view->zoom)
-			+ (view->x_shift / view->width);
-	y = ((4.0 * y / view->height - 2.0) / view->zoom)
-			+ (view->y_shift / view->height);
-	while (x * x + y * y < 4.0 && i < view->max_iter)
-	{
-		new_x = x * x * x * x - y * y * y * x * x -
-				(2 * x * x * y * y * y) - (2 * x * y * y) + view->fract->real;
-		y = (2 * x * x * x * y * y) - y * y * y * y -
-				(2 * x * x * y) + view->fract->imag;
-		x = new_x;
-		i++;
-	}
-	return (i);
-}
+/*
+**int				julia_quad(t_view *view, double x, double y)
+**{
+**	int			i;
+**	long double	new_x;
+**
+**	i = 0;
+**	x = ((4.0 * x / view->width - 2.0) / view->zoom)
+**			+ (view->x_shift / view->width);
+**	y = ((4.0 * y / view->height - 2.0) / view->zoom)
+**			+ (view->y_shift / view->height);
+**	while (x * x + y * y < 4.0 && i < view->max_iter)
+**	{
+**		new_x =
+**		y =
+**		x = new_x;
+**		i++;
+**	}
+**	return (i);
+**}
+*/
